95d1e747efe3398a1ebc0a75e07801624d5d893a
Jim Li <jimli101101@gmail.com>
2019-06-22 19:38:49 -0400

    Login Page working.
    Moved some files around and created a new folder to improve project organization.

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask
+"""
+Author: Jim Li
+Date: 22 June 2019
+
+Manages the portfolio manager website.
+"""
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+# Load the home page.
 @app.route("/")
 def load_index():
-    webPage = open("index.html", 'r')
-    htmlString = webPage.read()
-    webPage.close()
+    return render_template("index.html")
 
-    return htmlString
-
+# Handle user login
+@app.route("/login/", methods = ["GET", "POST"])
+def login_handler():
+    if request.method == "POST":
+        result = request.form
+        return render_template("login.html", result = result)
+    else:
+        return "<b> GET METHOD ERROR </b>"
+# Main Program Area
 if __name__ == "__main__":
     app.run()
