6b40f4b03e6624724dd623a7546c9d92ea495d08
Jim Li <jimli101101@gmail.com>
2019-06-24 20:13:16 -0400

    Add Account Creation Page + Docs
    Account creation page is created with some basic UI functionality 
    working. Additionally, I have documented some of the code using Doxygen, 
    specifically the Python file(s).

---
+++ html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['load_5findex',['load_index',['../namespaceapp.html#a0241d4e335a7865235ec20af9d0f4802',1,'app']]],
+  ['login_5fhandler',['login_handler',['../namespaceapp.html#a0761852417f8865627965bac62a81c66',1,'app']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['financial_2dportfolio_2dmanager',['Financial-Portfolio-Manager',['../md_README.html',1,'']]]
+];
